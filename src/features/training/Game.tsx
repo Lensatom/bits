b5030cb63ccf4b71ca846ad73f8860e22a8458d3
Lensatom <ayomidetommiwa@gmail.com>
2023-06-15 18:03:52 +0100

    UI corrections

--- src/features/training/Game.tsx
+++ src/features/training/Game.tsx
@@ -103,18 +103,18 @@ const Game = () => {
         </div>
         <div className='w-full h-[50%] flex flex-col items-start'>
           <div className="w-full h-full grid grid-cols-3 grid-rows-5 font-bold text-2xl gap-4 p-4 text-gray-900">
-            <button onClick={() => setUserAnswer(`${userAnswer}7`)} className='bg-gray-200 rounded-md'>7</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}8`)} className='bg-gray-200 rounded-md'>8</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}9`)} className='bg-gray-200 rounded-md'>9</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}4`)} className='bg-gray-200 rounded-md'>4</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}5`)} className='bg-gray-200 rounded-md'>5</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}6`)} className='bg-gray-200 rounded-md'>6</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}1`)} className='bg-gray-200 rounded-md'>1</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}2`)} className='bg-gray-200 rounded-md'>2</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}3`)} className='bg-gray-200 rounded-md'>3</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}-`)} className='bg-gray-200 rounded-md'>-</button>
-            <button onClick={() => setUserAnswer(`${userAnswer}0`)} className='bg-gray-200 rounded-md'>0</button>
-            <button onClick={() => setUserAnswer("")} className='bg-orange-200 text-gray-900 rounded-md'>C</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}7`)} className='keys rounded-md'>7</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}8`)} className='keys rounded-md'>8</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}9`)} className='keys rounded-md'>9</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}4`)} className='keys rounded-md'>4</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}5`)} className='keys rounded-md'>5</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}6`)} className='keys rounded-md'>6</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}1`)} className='keys rounded-md'>1</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}2`)} className='keys rounded-md'>2</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}3`)} className='keys rounded-md'>3</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}-`)} className='keys rounded-md'>-</button>
+            <button onClick={() => setUserAnswer(`${userAnswer}0`)} className='keys rounded-md'>0</button>
+            <button onClick={() => setUserAnswer("")} className='keys text-orange-600 rounded-md'>C</button>
             <button onClick={submitUserAnswer} className='bg-orange-600 text-white rounded-md col-span-3'>Enter</button>
           </div>
         </div>
