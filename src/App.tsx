2ec1bbf4dfc5b5b890aeb13de2a6924e6066caa1
Lensatom <ayomidetommiwa@gmail.com>
2023-07-25 21:07:43 +0100

    Setting environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { GetUser, SaveRoom } from './redux/action';
 import { GetData } from './firebase/firestore';
 import { Horj, Host, Join, Game as MultiGame, Room } from './features/multi';
 import { Loader } from './components';
+import { MdWarning } from 'react-icons/md';
 
 function App() {
 
@@ -36,34 +37,42 @@ function App() {
     })
   }, [])
 
-  if (!loaded) {
+  if (window.innerWidth > 500) {
+    return (
+      <div className='w-full h-screen flex flex-col gap-2 justify-center items-center text-xl font-medium text-gray-600'>
+        <MdWarning className="text-4xl" />
+        Uclid works best on a mobile device
+        <p className='text-sm font-normal'>Open this site on your phone</p>
+      </div>
+    )
+  } else if (loaded) {
+    return (
+      <div className='bg-white'>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="selectgame" element={<SelectGame />} />
+          <Route path="train">
+            <Route path="game" element={<Game />} />
+          </Route>
+          <Route path="multi">
+            <Route path="horj" element={<Horj />} />
+            <Route path="host" element={<Host />} />
+            <Route path="join" element={<Join />} />
+            <Route path="room" element={<Room />} />
+            <Route path="game" element={<MultiGame />} />
+          </Route>
+        </Routes>
+      </div>
+    )
+  } else {
     return (
       <div className='w-full h-screen'>
         <Loader />
       </div>
     )
   }
-
-  return (
-    <div className='bg-white'>
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="selectgame" element={<SelectGame />} />
-        <Route path="train">
-          <Route path="game" element={<Game />} />
-        </Route>
-        <Route path="multi">
-          <Route path="horj" element={<Horj />} />
-          <Route path="host" element={<Host />} />
-          <Route path="join" element={<Join />} />
-          <Route path="room" element={<Room />} />
-          <Route path="game" element={<MultiGame />} />
-        </Route>
-      </Routes>
-    </div>
-  )
 }
 
 export default App
--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHR66SPrCCxyM2niiGRTOx86DNQOYHlnY",
-  authDomain: "uclid-30537.firebaseapp.com",
-  projectId: "uclid-30537",
-  storageBucket: "uclid-30537.appspot.com",
-  messagingSenderId: "825241354186",
-  appId: "1:825241354186:web:1483d09912f92059395933",
-  measurementId: "G-TJ5HPFQ9L7"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig);
